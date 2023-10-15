6de7c71e83815fb6da5c3ef24aeef96ade29086c
r.trofimenko <r.trofimenko@net-vision.pro>
2023-10-15 12:16:37 +0300

    fixed path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cifrovoi_proriv_sochi/',
+  base: '/cifrovoi_proriv_soch/',
   plugins: [
     vue(),
   ],
